77b094d3aed0c63eef77867e33afd24188fa818f
lucapolzone <lucapolzone@gmail.com>
2024-04-05 16:50:40 +0200

    feat: add foreign key for 'type_id' column in 'projects' table"

--- database/migrations/2024_04_02_141343_create_projects_table.php
+++ database/migrations/2024_04_02_141343_create_projects_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            // $table->foreignId('type_id')->constrained(); //forma contratta
             $table->string('title', 150);
             $table->text('content');
             $table->string('link');
