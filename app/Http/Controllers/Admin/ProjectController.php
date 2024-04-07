8f177cf47ea220b68324ab75abf511092acd4887
lucapolzone <lucapolzone@gmail.com>
2024-04-07 14:16:21 +0200

    feat: add create() for Type resource

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -27,7 +28,9 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        return view('admin.projects.create');
+        $project =  new Project;
+        $types =  Type::all();
+        return view('admin.projects.create', compact('project', 'types'));
     }
 
     /**
